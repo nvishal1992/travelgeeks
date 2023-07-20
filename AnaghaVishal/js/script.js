e66e07b3a602ff443c907729281c90ec29c57c62
nvishal1992 <nvishal1992@gmail.com>
2023-07-20 16:39:10 +0530

    Update script.js

--- AnaghaVishal/js/script.js
+++ AnaghaVishal/js/script.js
@@ -1,29 +1,18 @@
-// Get the target date (2nd Jan 2023, 7:54:06 PM)
-const targetDate = new Date(2023, 0, 2, 19, 54, 6);
+// Get the target date (replace with your desired start date and time)
+const targetDate = new Date('2023-01-02T19:54:06').getTime();
 
-// Update the time lapsed every second
-const timeLapsedInterval = setInterval(function() {
-    const now = new Date();
+// Update the time elapsed every second
+const timeElapsedInterval = setInterval(function() {
+    const now = new Date().getTime();
     const timeElapsed = now - targetDate;
 
     // Calculate time units
-    const years = Math.floor(timeElapsed / (365.25 * 24 * 60 * 60 * 1000));
-    const days = Math.floor(timeElapsed / (24 * 60 * 60 * 1000));
-    const months = Math.floor(days / 30.44); // Approximation based on an average month
-    const remainingDays = days % 30.44;
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    const days = Math.floor(timeElapsed / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeElapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeElapsed % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeElapsed % (1000 * 60)) / 1000);
 
-    // Format the output as "dd-mm-yyyy hh:mm:ss"
-    const formattedTime = `${padZero(now.getDate())}-${padZero(now.getMonth() + 1)}-${now.getFullYear()} ${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
-
-    // Display the time lapsed on the webpage
-    const timeLapsedElement = document.getElementById('time-lapsed');
-    timeLapsedElement.innerHTML = `${years} years, ${months} months, ${remainingDays} days, ${formattedTime}`;
+    // Display the time elapsed on the webpage
+    const timeElapsedElement = document.getElementById('time-elapsed');
+    timeElapsedElement.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds.`;
 }, 1000);
-
-// Function to pad single-digit numbers with a leading zero
-function padZero(num) {
-    return num.toString().padStart(2, '0');
-}
