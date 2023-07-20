d57550bbfca9ab049e013e0feb60e8605e2dfcac
nvishal1992 <nvishal1992@gmail.com>
2023-07-20 17:03:16 +0530

    Update script.js

--- AnaghaVishal/js/script.js
+++ AnaghaVishal/js/script.js
@@ -1,27 +1,33 @@
-// Get the target date (January 2nd, 2023, at 7:54:06 PM)
-const targetDate = new Date('2023-01-02T19:54:06').getTime();
+// Sample baby photo list (you can replace these URLs with actual photo URLs)
+const babyPhotos = [
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+];
 
-// Update the time elapsed every second
-const timeElapsedInterval = setInterval(function() {
-    const now = new Date().getTime();
-    const timeElapsed = now - targetDate;
+// Populate the photo list with baby photos
+const photoListElement = document.getElementById('photo-list');
+babyPhotos.forEach(photoUrl => {
+    const listItem = document.createElement('li');
+    const photo = document.createElement('img');
+    photo.src = photoUrl;
+    listItem.appendChild(photo);
+    photoListElement.appendChild(listItem);
+});
 
-    // Calculate time units
-    const months = Math.floor(timeElapsed / (30.44 * 24 * 60 * 60 * 1000)); // Approximation based on an average month
-    const remainingTime = timeElapsed % (30.44 * 24 * 60 * 60 * 1000);
-    const days = Math.floor(remainingTime / (24 * 60 * 60 * 1000));
-    const remainingTimeInDays = remainingTime % (24 * 60 * 60 * 1000);
-    const hours = Math.floor(remainingTimeInDays / (60 * 60 * 1000));
-    const remainingTimeInHours = remainingTimeInDays % (60 * 60 * 1000);
-    const minutes = Math.floor(remainingTimeInHours / (60 * 1000));
-    const seconds = Math.floor((remainingTimeInHours % (60 * 1000)) / 1000);
+// Function to capture the event details for a photo
+function captureEvent() {
+    const eventNameInput = document.getElementById('eventName');
+    const eventDateInput = document.getElementById('eventDate');
+    const selectedPhoto = document.querySelector('.selected');
 
-    // Display the time elapsed on the webpage
-    const timeElapsedElement = document.getElementById('time-lapsed');
-    timeElapsedElement.innerHTML = `${months} months, ${days} days, ${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
-}, 1000);
+    if (selectedPhoto && eventNameInput.value && eventDateInput.value) {
+        const eventDetails = document.createElement('div');
+        eventDetails.innerHTML = `<strong>${eventNameInput.value}</strong>: ${eventDateInput.value}`;
+        selectedPhoto.appendChild(eventDetails);
 
-// Function to pad single-digit numbers with a leading zero
-function padZero(num) {
-    return num.toString().padStart(2, '0');
+        // Clear the input fields after capturing the event
+        eventNameInput.value = '';
+        eventDateInput.value = '';
+    }
 }
