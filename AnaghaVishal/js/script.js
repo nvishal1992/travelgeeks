5612fb42ce8517117d7c8226bd12c6d383646e16
nvishal1992 <nvishal1992@gmail.com>
2023-07-20 16:51:17 +0530

    Update script.js

--- AnaghaVishal/js/script.js
+++ AnaghaVishal/js/script.js
@@ -1,4 +1,4 @@
-// Get the target date (replace with your desired start date and time)
+// Get the target date and time (January 2nd, 2023, 7:54:06 PM)
 const targetDate = new Date('2023-01-02T19:54:06').getTime();
 
 // Update the time elapsed every second
@@ -7,12 +7,20 @@ const timeElapsedInterval = setInterval(function() {
     const timeElapsed = now - targetDate;
 
     // Calculate time units
-    const days = Math.floor(timeElapsed / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeElapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeElapsed % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeElapsed % (1000 * 60)) / 1000);
+    const millisecondsInSecond = 1000;
+    const millisecondsInMinute = millisecondsInSecond * 60;
+    const millisecondsInHour = millisecondsInMinute * 60;
+    const millisecondsInDay = millisecondsInHour * 24;
+    const millisecondsInMonth = millisecondsInDay * 30.44; // Approximation based on an average month
+
+    const months = Math.floor(timeElapsed / millisecondsInMonth);
+    const remainingTime = timeElapsed % millisecondsInMonth;
+    const days = Math.floor(remainingTime / millisecondsInDay);
+    const hours = Math.floor((remainingTime % millisecondsInDay) / millisecondsInHour);
+    const minutes = Math.floor((remainingTime % millisecondsInHour) / millisecondsInMinute);
+    const seconds = Math.floor((remainingTime % millisecondsInMinute) / millisecondsInSecond);
 
     // Display the time elapsed on the webpage
-    const timeElapsedElement = document.getElementById('time-elapsed');
-    timeElapsedElement.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds.`;
+    const timeElapsedElement = document.getElementById('time-lapsed');
+    timeElapsedElement.innerHTML = `${months} months, ${days} days, ${hours}h ${minutes}m ${seconds}s`;
 }, 1000);
